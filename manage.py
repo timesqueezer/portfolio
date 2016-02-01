b1d9d869d4973adc36c61dacb39f9e3ce7c7e45c
Matz Radloff <matzradloff@gmail.com>
2016-02-01 17:12:53 +0100

    Added manage script for assets and ansible stuff.

---
+++ manage.py
@@ -0,0 +1,9 @@
+#!env/bin/python
+from flask.ext.script import Manager
+from flask.ext.assets import ManageAssets
+
+from portfolio import create_app
+
+manager = Manager(create_app)
+manager.add_command("assets", ManageAssets)
+
